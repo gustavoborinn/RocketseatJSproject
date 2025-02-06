7f5cb0e22cb8efc41fe04e29f229dea9d307df26
gustavoborinn <gustavo.borin04@gmail.com>
2025-02-06 19:11:07 -0300

    Show footer with currency description

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const BTC = 557072.64
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
 
 // Manipula o input amount para receber só numero
 amount.addEventListener("input" , () => {
@@ -37,5 +38,12 @@ form.onsubmit = (event) => {
 
 // Função para converter a moeda
 function convertCurrency(amount, price, symbol){
-  console.log(amount, price, symbol)
+  try {
+    footer.classList.add("show-result") 
+  } catch (error) {
+    console.log(error)
+  
+    footer.classList.remove("show-result")
+    alert("Não foi possível converter. Tente novamente mais tarde.")
+  }
 }
